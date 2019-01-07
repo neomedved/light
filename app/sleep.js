2d9e9ccfb517c1cf2332b620c3183129187f1891
neomedved <at.medvedev@gmail.com>
2019-01-07 11:52:01 +0300

    bug fix 2

--- app/sleep.js
+++ app/sleep.js
@@ -3,4 +3,4 @@ const app_domain = 'https://light-in-house.herokuapp.com'
 
 setInterval( () => {
     https.get(app_domain)
-}, 1500000) // every 25 minutes (1500000)
+}, 500000) // every 5 minutes (500000)
