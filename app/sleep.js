76d24751f8e62b5dadcd60fb67daf52b699b19c5
neomedved <at.medvedev@gmail.com>
2019-01-08 10:28:04 +0300

    sleep interval change

--- app/sleep.js
+++ app/sleep.js
@@ -3,4 +3,4 @@ const app_domain = 'https://light-in-house.herokuapp.com'
 
 setInterval( () => {
     https.get(app_domain)
-}, 500000) // every 5 minutes (500000)
+}, 600000) // every 10 minutes (600000)
